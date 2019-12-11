c6089396aa6e43ef9d9c9f1cae8ba4d1993362e0
marioVega <mario.vega003@mymdc.net>
2019-12-11 03:16:34 -0500

    adding first repo in intelli j

---
+++ src/main/java/academy/learnprograming/HelloMaven.java
@@ -0,0 +1,12 @@
+package academy.learnprograming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloMaven {
+    private final static Logger log = LoggerFactory.getLogger(HelloMaven.class);
+    public static void main(String[] args) {
+        log.info("Hello info");
+        log.debug("Hello debug");
+    }
+}
